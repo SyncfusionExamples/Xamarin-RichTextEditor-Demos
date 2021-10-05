6776df146284abe90b74b5863f5caa1ad9d94fde
Prakash Kumar D <prakashd@syncfusion.com>
2021-10-05 19:07:21 +0530

    How-to-use-Xamarin.Forms-RTE-in-Xamarin.Android-and-Xamarin.iOS-platforms

---
+++ How-to-use-Xamarin.Forms-RTE-in-Xamarin.Android-and-Xamarin.iOS-platforms/SfRTEAndroid/SfRTE/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Xamarin.Forms;
+
+using Xamarin.Forms.Platform.Android;
+namespace SfRTE
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Forms.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var mainPage = new SfRTE().CreateSupportFragment(this);
+            SupportFragmentManager
+             .BeginTransaction()
+             .Replace(Resource.Id.fragment_frame_layout, mainPage)
+             .Commit();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
